c76f9a02d05bf9b2ddc6535d9a3b58ec6b850d18
ryowuandjanet <ryowu0329@gmail.com>
2024-04-29 15:54:29 +0000

    03

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/users", (req, res) => {
-    res.send("All Users");
+router.get("/", (req, res) => {
+    res.render("index",{ title: "首頁"});
+})
+
+router.get("/add", (req, res) => {
+    res.render("add_users",{ title: "新增成員"});
 })
 
 module.exports = router;
