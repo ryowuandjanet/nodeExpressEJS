74be59c577ea166366f89144c86b958c1a8af9b6
ryowuandjanet <ryowu0329@gmail.com>
2024-04-30 17:02:24 +0000

    05

--- routes/routes.js
+++ routes/routes.js
@@ -12,10 +12,10 @@ if (!fs.existsSync(uploadDirectory)) {
 }
 
 var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: function (req, file, cb) {
         cb(null, "./uploads");
     },
-    filename: function(req, file, cb) {
+    filename: function (req, file, cb) {
         cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
     }
 });
@@ -28,7 +28,7 @@ router.post("/add", upload, async (req, res) => {
     if (!req.file) {
         return res.status(400).json({ message: "No file uploaded.", type: "danger" });
     }
-    
+
     try {
         const user = new User({
             name: req.body.name,
@@ -39,7 +39,7 @@ router.post("/add", upload, async (req, res) => {
         await user.save();
         req.session.message = {
             type: 'success',
-            message: 'User added successfully!'
+            message: '成功建立使用者!'
         };
         return res.redirect("/");
     } catch (error) {
@@ -58,6 +58,101 @@ router.get("/", async (req, res) => {
 
 router.get("/add", (req, res) => {
     res.render("add_users", { title: "新增成員" });
+});
+
+
+router.get("/edit/:id", async (req, res) => {
+    try {
+        let id = req.params.id;
+        let user = await User.findById(id).exec();
+
+        if (user == null) {
+            return res.redirect("/");
+        }
+
+        res.render("edit_users", {
+            title: "Edit User",
+            user: user,
+        });
+    } catch (err) {
+        console.error(err);
+        res.redirect("/");
+    }
+});
+
+
+// Update user route
+router.post("/update/:id", upload, async (req, res) => {
+    try {
+        let id = req.params.id;
+        let new_image = "";
+        if (req.file) {
+            new_image = req.file.filename;
+            try {
+                fs.unlinkSync("./uploads/" + req.body.old_image);
+            } catch (err) {
+                console.log(err);
+            }
+        } else {
+            new_image = req.body.old_image;
+        }
+
+        const result = await User.findByIdAndUpdate(
+            id,
+            {
+                name: req.body.name,
+                email: req.body.email,
+                phone: req.body.phone,
+                image: new_image,
+            },
+            { new: true } // This option returns the updated document
+        );
+
+        if (!result) {
+            return res.json({ message: "User not found", type: "danger" });
+        }
+
+        req.session.message = {
+            type: "success",
+            message: "User updated successfully!",
+        };
+        res.redirect("/");
+    } catch (err) {
+        console.error(err);
+        res.json({ message: err.message, type: "danger" });
+    }
+});
+
+
+// Delete user route
+router.get('/delete/:id', async (req, res) => {
+    try {
+        let id = req.params.id;
+        let user = await User.findById(id);
+
+        if (!user) {
+            return res.json({ message: "User not found" });
+        }
+
+        if (user.image !== '') {
+            try {
+                fs.unlinkSync('./uploads/' + user.image);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        await user.deleteOne();
+
+        req.session.message = {
+            type: 'info',
+            message: 'User deleted successfully!'
+        };
+        res.redirect("/");
+    } catch (err) {
+        console.error(err);
+        res.json({ message: err.message });
+    }
 });
 
 module.exports = router;
